4d78a80b082d2fc1b30ce4b798b5dba7085fa49a
federicamts <federica.montesano42@gmail.com>
2025-06-12 11:23:01 +0200

    refactor(minhash-serial.c)
    refactored method insert
    refactored data types in method query

--- src/serial/minhash-serial.c
+++ src/serial/minhash-serial.c
@@ -3,13 +3,13 @@
 #include <configuration.h>
 
 
-void insert(minhash_sketch *minhashsketch, uint64_t elem) {
+void insert(minhash_sketch *sketch, uint64_t elem) {
 
 	uint64_t i;
-	for (i = 0; i < minhashsketch->size; i++) {
-		uint64_t val = minhashsketch->hash_functions[i].hash_function(&minhashsketch->hash_functions[i], elem);
-		if (val < minhashsketch->sketch[i])
-			minhashsketch->sketch[i] = val;
+	for (i = 0; i < sketch->size; i++) {
+		uint64_t val = sketch->hash_functions[i].hash_function(&sketch->hash_functions[i], elem);
+		if (val < sketch->sketch[i])
+			sketch->sketch[i] = val;
 	}
 
 }
@@ -19,7 +19,7 @@ void insert(minhash_sketch *minhashsketch, uint64_t elem) {
 float query(minhash_sketch *sketch, minhash_sketch *otherSketch) {
 
 	uint64_t i;
-	uint64_t count = 0;
+	float count = 0.f;
 	for (i=0; i < sketch->size; i++) {
 		if (IS_EQUAL(sketch->sketch[i], otherSketch->sketch[i]))
 			count++;
