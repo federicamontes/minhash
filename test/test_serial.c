a9518c1c6eb45b5b0e038e1b26af55579de4533b
federicamts <federica.montesano42@gmail.com>
2025-06-12 11:23:27 +0200

    featuer(test_serial)
    added unit test for serial version of minhash
    
    TODO fix bug for similarity of sketches regarding casting of variables

--- test/test_serial.c
+++ test/test_serial.c
@@ -3,13 +3,42 @@
 
 int main() {
     minhash_sketch *sketch;
-    minhash_init(&sketch, 128, 0);
+    minhash_sketch *sketch2;
+
+
+
+    pairwise_hash *hash_functions = malloc(128 * sizeof(pairwise_hash));
+    if (hash_functions == NULL) {
+        fprintf(stderr, "Error in malloc() when allocating hash functions\n");
+        exit(1);
+    }
+
+    
+    hash_functions_init(hash_functions, 128);
+
+
+    minhash_init(&sketch, hash_functions, 128, 50);
+    minhash_init(&sketch2, hash_functions, 128, 100);
 
     insert(sketch, 42);
     insert(sketch, 99);
 
-    float sim = query(sketch, sketch);
-    printf("Self-similarity: %.2f\n", sim);
+    uint64_t i;
+    for (i = 0; i < 128; i++) {
+        fprintf(stderr, "sketch 1 values: %lu \t", sketch->sketch[i]);
+    }
+    fprintf(stderr, "\n");
+
+    insert(sketch2, 42);
+    insert(sketch2, 100);  // different element to differ the sketches
+
+    for (i = 0; i < 128; i++) {
+        fprintf(stderr, "sketch 2 values: %lu \t", sketch2->sketch[i]);
+    }
+    fprintf(stderr, "\n");
+
+    float sim = query(sketch, sketch2);
+    printf("Similarity: %.2f\n", sim);
 
     minhash_free(sketch);
     return 0;
