820e777b481cca6a9f42b822ecaf71a9d53c38d1
federicamts <federica.montesano42@gmail.com>
2025-06-12 11:22:06 +0200

    refactor(configuration.c)
    refactored allocation in init methods

--- src/configuration/configuration.c
+++ src/configuration/configuration.c
@@ -12,6 +12,7 @@ void hash_functions_init(pairwise_hash *hf, uint64_t size) {
     }
 }
 
+
 void init_empty_values(minhash_sketch *sketch) {
 
 	uint64_t i;
@@ -19,15 +20,15 @@ void init_empty_values(minhash_sketch *sketch) {
 		sketch->sketch[i] = INFTY;
 }
 
-void init_values(minhash_sketch **sketch, uint64_t size) {
+void init_values(minhash_sketch *sketch, uint64_t size) {
 
 	uint64_t i;
 	for (i=0; i < size; i++) {
-		insert(*sketch, i);
+		insert(sketch, i);
 	}
 }
 
-void minhash_init(minhash_sketch **sketch, uint64_t sketch_size, int init_size) {
+void minhash_init(minhash_sketch **sketch, pairwise_hash *hash_functions, uint64_t sketch_size, int init_size) {
     
     *sketch = malloc(sizeof(minhash_sketch));
     if (*sketch == NULL) {
@@ -44,22 +45,23 @@ void minhash_init(minhash_sketch **sketch, uint64_t sketch_size, int init_size) 
         exit(1);
     }
 
-    (*sketch)->hash_functions = malloc(sketch_size * sizeof(pairwise_hash));
+    /*(*sketch)->hash_functions = malloc(sketch_size * sizeof(pairwise_hash));
     if ((*sketch)->hash_functions == NULL) {
         fprintf(stderr, "Error in malloc() when allocating hash functions\n");
         exit(1);
     }
 
     
-    hash_functions_init((*sketch)->hash_functions, sketch_size);
+    hash_functions_init((*sketch)->hash_functions, sketch_size);*/
+	(*sketch)->hash_functions = hash_functions;
 
-    
+
     init_empty_values(*sketch);
 
 
     
     if (init_size > 0)
-        init_values(sketch, init_size);
+        init_values(*sketch, init_size);
 
 
 }
