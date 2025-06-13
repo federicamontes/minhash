22fd9bf06afeddbcdc6563b495545f1de9425456
federicamts <federica.montesano42@gmail.com>
2025-06-13 13:15:47 +0200

    feature(hash function)
    added implementation of new hash functions, kwise hash function

--- include/hash.h
+++ include/hash.h
@@ -2,17 +2,27 @@
 #define HASH_H
 
 #include <stdint.h>
+#include <assert.h>
 
 typedef struct pairwise_hash { ///TODO change struct name to allow different hash functions by configuration
-	uint64_t a; /// first coefficient
-	uint64_t b; /// second coefficient
+	uint32_t a; /// first coefficient
+	uint32_t b; /// second coefficient
 	uint64_t M; /// this is a large prime number
 	uint64_t (*hash_function)(struct pairwise_hash *self, uint64_t x);
 } pairwise_hash;
 
 
-extern pairwise_hash *hash_funcs;
-uint64_t pairwise_func(pairwise_hash *self, uint64_t x);
+typedef struct kwise_hash {
+	uint32_t k; /// polynomial degree
+	uint64_t M; /// modulus
+	uint32_t *coefficients; /// array of coefficients
+	uint64_t (*hash_function) (struct kwise_hash *self, uint64_t x);
+} kwise_hash;
 
+
+extern pairwise_hash *p_hash_funcs;
+extern kwise_hash *k_hash_funcs;
+uint64_t pairwise_func(pairwise_hash *self, uint64_t x);
+uint64_t kwise_func(kwise_hash *self, uint64_t x);
 
 #endif
--- src/utils/hash.c
+++ src/utils/hash.c
@@ -2,8 +2,24 @@
 #include <hash.h>
 
 
-// Actual hash function definition
+// Pairwise hash function implementation
 uint64_t pairwise_func(pairwise_hash *self, uint64_t x) {
-    return (self->a * x + self->b) % self->M;
+    assert(self->M > 0);
+    return ((self->a * x % self->M) + self->b) % self->M;
 }
 
+
+/// Kwise hash function implementation
+uint64_t kwise_func(kwise_hash *self, uint64_t x) {
+
+    uint64_t pow_x = 1;
+    uint64_t sum = 0;
+    uint32_t i = 0;
+
+    for (i = 0; i <= self->k; i++) {
+        sum = (sum + (pow_x * self->coefficients[i] % self->M)) % self->M;
+        pow_x = (pow_x * x) % self->M;
+    }
+
+    return sum % self->M;
+}
