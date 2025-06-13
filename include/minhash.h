b61bf0aaeacc79e4389c9a6042f3ae4c7348d321
federicamts <federica.montesano42@gmail.com>
2025-06-13 15:53:03 +0200

    feature(locks)
    added definition of locks

--- include/minhash.h
+++ include/minhash.h
@@ -6,6 +6,9 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <stdio.h>
+#if defined(LOCKS) || defined(RW_LOCKS)
+    #include <pthread.h>
+#endif
 
 #include <hash.h>
 
@@ -19,6 +22,16 @@ typedef struct minhash_sketch {
 	uint64_t *sketch;
 	uint32_t hash_type;
 	void *hash_functions;
+#ifdef LOCKS
+	pthread_mutex_t lock;
+#endif
+#ifdef RW_LOCKS
+/** rw_locks guarantee that while a write_lock is acquired, no readers or writers can access the locked sketch
+ *  when one or more readers acquire the read_lock, no writer can acquire the write lock until ALL the reader
+ *  have released the lock
+*/
+	pthread_rwlock_t rw_lock;
+#endif
 } minhash_sketch;
 
 extern minhash_sketch *sketch;
@@ -35,5 +48,9 @@ void minhash_free(minhash_sketch *mh);
 /** SKETCH OPERATIONS */
 void insert(minhash_sketch *sketch, uint64_t elem);
 float query(minhash_sketch *sketch, minhash_sketch *otherSketch);
+
+
+void insert_parallel(minhash_sketch *sketch, uint64_t elem);
+float query_parallel(minhash_sketch *sketch, minhash_sketch *otherSketch);
 
 #endif
