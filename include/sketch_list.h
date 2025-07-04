b3839d49544f91408e7172eaf8b4e85a5c408665
federicamts <federica.montesano42@gmail.com>
2025-07-04 14:35:15 +0200

    feature(sketch list)
    created header file for sketch list with declaration of struct and method

--- include/sketch_list.h
+++ include/sketch_list.h
@@ -7,9 +7,16 @@
 
 #include <utils.h>
 
-// TODO struct of list 
+// TODO struct of list
+typedef struct sketch_record {
+	_Atomic unsigned long *versioned;   // versioned[0] is the address of next record in the list
+										// versioned[1] is the counter of ongoing readers
+	uint64_t *sketch_version;
+
+} sketch_record;
 
 // TODO methods for managing list
+void create_and_push_new_node(_Atomic unsigned long** head_sl, uint64_t *version_sketch, uint64_t size);
 
-
+	
 #endif
