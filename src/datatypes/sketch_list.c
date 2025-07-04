f150d9c35c0cbcaf3eb024a4f30eb92f2c5c664d
federicamts <federica.montesano42@gmail.com>
2025-07-04 14:35:46 +0200

    feature(sketch_list)
    started implementation of sketch list
    to be tested

--- src/datatypes/sketch_list.c
+++ src/datatypes/sketch_list.c
@@ -2,3 +2,26 @@
 
 
 // TODO methods implementation for managing list LIFO
+void create_and_push_new_node(_Atomic unsigned long** head_sl, uint64_t *version_sketch, uint64_t size) {
+
+	sketch_record *new_node = malloc(sizeof(sketch_record));
+	if (new_node == NULL) {
+		fprintf(stderr, "Error in creating new node for sketch list \n");
+		exit(1);
+	}
+
+	_Atomic unsigned long *new_versioned = malloc(sizeof(unsigned long) * 2);
+	if (new_versioned == NULL) {
+		fprintf(stderr, "Error in allocating new versioned field of new record in sketch list \n");
+		exit(1);
+	}
+
+	new_node->sketch_version = version_sketch;
+
+	_Atomic unsigned long *p;
+	sketch_record *record = ((sketch_record *) new_versioned[0]); 
+	p = head_sl;
+	head_sl = DWCAS(head_sl, p, new_versioned);
+	record->versioned = *head_sl;
+	
+}
