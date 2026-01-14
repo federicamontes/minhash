674fef59ae7c951abb91c3b9935c967f12124c13
federicamontes <federica.montesano42@gmail.com>
2026-01-14 09:42:26 +0100

    bugfix for script run tests prob

--- run_tests_prob.sh
+++ run_tests_prob.sh
@@ -50,7 +50,8 @@ for THREADS in "${THREAD_COUNTS[@]}"; do
     # fcds test
     if [ "$THREADS" -eq 2 ]; then
         echo "Skipping FCDS test for thread count: 2"
-    else
+        continue
+        
     for WP in "${WRITE_PROBS[@]}"; do
         for ((RUN=1; RUN<=NUM_RUNS; RUN++)); do
             OUT_FILE="${OUTPUT_DIR}/fcds_prob_ops${NUM_OPS}_size${SKETCH_SIZE}_init${INITIAL_SIZE}_b${THRESHOLD_INSERTION}_wp${WP}_threads${THREADS}_run${RUN}.txt"
