17529f564aeeb6f1783a1145bed4d3b66da7bcb6
Luca Pepè Sciarria <luca.pepesciarria@gmail.com>
2026-01-23 15:40:49 +0100

    remove print

--- src/serial/minhash-serial.c
+++ src/serial/minhash-serial.c
@@ -18,9 +18,10 @@ float query(minhash_sketch *sketch, minhash_sketch *otherSketch) {
 	uint64_t i;
 	int count = 0;
 	for (i=0; i < sketch->size; i++) {
+        //fprintf(stderr, "[query]  %d ? %d\n", sketch->sketch[i], otherSketch->sketch[i]);
 		if (IS_EQUAL(sketch->sketch[i], otherSketch->sketch[i]))
 			count++;
 	}
-    //fprintf(stderr, "[query] actual count %d\n", count);
+   // fprintf(stderr, "[query] actual count %d\n", count);
 	return count/(float)sketch->size;
 }
